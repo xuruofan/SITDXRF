cdc99915cdb6764a765c613a9ff78ca7dc239415
Xu Ruofan <danceey@hotmail.com>
2018-12-07 18:01:11 +0800

    Added Event

---
+++ UISystem/Assets/Shimmer/Common/Scripts/Event.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Shimmer.Common
+{
+	public delegate void Callback();
+
+	public class Event
+	{
+		private Dictionary<Callback, int> m_listeners = new Dictionary<Callback, int>();
+		private bool m_isRaised = false;
+
+		public void Raise()
+		{
+
+		}
+
+		public void Subscribe(Callback _callback)
+		{
+
+		}
+
+		public void Unsubscribe(Callback callback)
+		{
+
+		}
+	}
+}
---
+++ UISystem/Assets/Shimmer/Common/Scripts/VariableOf.cs
@@ -0,0 +1,13 @@
+﻿namespace Shimmer.Common
+{
+	public interface IVariable<T>
+	{
+		Event Changed { get; }
+
+		T GetValue();
+		void SetValue(T _value);
+		void Subscribe();
+		void Unsubscribe();
+		void Raise();
+	}
+}
