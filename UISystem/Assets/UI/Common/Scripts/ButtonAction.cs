e93d2920bc0c7e8a6115623261ceaff096d66ed8
xuruofan <xuruofan@users.noreply.github.com>
2018-12-16 21:48:12 +0800

    Implemented ButtonAction
    Link a actionlist to a Button component's onclick event

---
+++ UISystem/Assets/UI/Common/Scripts/ButtonAction.cs
@@ -0,0 +1,37 @@
+﻿using Shimmer.Common.Actions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Shimmer.UI.Common
+{
+	[RequireComponent(typeof(Button))]
+	public class ButtonAction : MonoBehaviour
+	{
+		public ActionList OnButtonClick;
+
+		private Button m_Button;
+
+		private void Awake()
+		{
+			m_Button = gameObject.GetComponent<Button>();
+		}
+
+		private void OnEnable()
+		{
+			m_Button.onClick.AddListener(OnClick);
+		}
+
+		private void OnDisable()
+		{
+			m_Button.onClick.RemoveListener(OnClick);
+		}
+
+		private void OnClick()
+		{
+			OnButtonClick.Execute();
+		}
+	}
+}
