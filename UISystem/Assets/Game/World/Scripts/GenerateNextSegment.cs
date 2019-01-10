eecb431fabc061bfbd5c397ec55feebacf7c13b4
xuruofan <xuruofan@users.noreply.github.com>
2019-01-10 22:33:52 +0800

    Generate next segment
    Update camera and background height according to player height

---
+++ UISystem/Assets/Game/World/Scripts/GenerateNextSegment.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Shimmer.Game.World
+{
+	[RequireComponent(typeof(Collider2D))]
+	public class GenerateNextSegment : MonoBehaviour
+	{
+		public RectTransform NextSpawnPoint;
+		public GameObject Prefab;
+
+		private void OnTriggerEnter2D(Collider2D collision)
+		{
+			if(collision.gameObject.tag == "Player")
+			{
+				GameObject nextSegObject = Instantiate<GameObject>(Prefab);
+				nextSegObject.transform.position = NextSpawnPoint.position;
+			}
+		}
+	}
+}
---
+++ UISystem/Assets/Game/World/Scripts/UpdateBackgroundHeight.cs
@@ -0,0 +1,24 @@
+﻿using Shimmer.Common.Variables;
+using UnityEngine;
+
+namespace Shimmer.Game.World
+{
+	public class UpdateBackgroundHeight : MonoBehaviour
+	{
+		public FloatReference PlayerHeight;
+
+		private void OnEnable()
+		{
+			Vector3 pos = gameObject.transform.position;
+			pos.y = PlayerHeight.GetValue();
+			if (pos.y < 0)
+			{
+				pos.y = 0;
+			}
+
+			gameObject.transform.position = pos;
+
+			enabled = false;
+		}
+	}
+}
