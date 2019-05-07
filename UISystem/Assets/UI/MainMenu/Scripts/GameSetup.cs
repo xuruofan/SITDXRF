d631dcf89058325cd684270142311dc4504ba0b1
xuruofan <xuruofan@users.noreply.github.com>
2019-05-07 22:07:47 +0800

    Animations, transitions and game scene

---
+++ UISystem/Assets/UI/MainMenu/Scripts/GameSetup.cs
@@ -0,0 +1,15 @@
+﻿using Shimmer.Common.Actions;
+using UnityEngine;
+
+namespace Shimmer.UI.Common
+{
+    public class GameSetup : MonoBehaviour
+    {
+		public ActionList Setup;
+
+		private void Awake()
+		{
+			Setup.Execute(this);
+		}
+	}
+}
